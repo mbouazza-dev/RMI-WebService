0fbe924a42ceb08a00809f550d07038e9f706c40
yohan_rameau <yohan.rameau@gmail.com>
2021-11-20 14:22:41 +0100

    [Mail Notification] add JavaMailSender Bean in AppConfig

--- eiffel-corp/src/fr/uge/main/AppConfiguration.java
+++ eiffel-corp/src/fr/uge/main/AppConfiguration.java
@@ -4,9 +4,12 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.Properties;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import fr.sharedclasses.IStore;
 import fr.uge.employee.EmployeesDB;
@@ -17,6 +20,24 @@ public class AppConfiguration {
 	@Bean
 	public EmployeesDB getEmployeesDB() {
 		return new EmployeesDB();
+	}
+	
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+	    mailSender.setHost("smtp.gmail.com");
+	    mailSender.setPort(587);
+	    
+	    mailSender.setUsername("eiffel_corp.gmail@gmail.com");
+	    mailSender.setPassword("password");
+	    
+	    Properties props = mailSender.getJavaMailProperties();
+	    props.put("mail.transport.protocol", "smtp");
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.starttls.enable", "true");
+	    props.put("mail.debug", "true");
+	    
+	    return mailSender;
 	}
 	
 	@Bean
