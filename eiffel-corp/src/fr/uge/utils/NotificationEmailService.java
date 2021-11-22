2341690b8118a11f1f25dc0e4ef9e9f3690718d4
yohan_rameau <yohan.rameau@gmail.com>
2021-11-22 12:10:42 +0100

    [Mail Notification] implement email service on the notification observer.

--- eiffel-corp/src/fr/uge/utils/NotificationEmailService.java
+++ eiffel-corp/src/fr/uge/utils/NotificationEmailService.java
@@ -1,22 +1,26 @@
 package fr.uge.utils;
 
+import java.rmi.RemoteException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
+import fr.sharedclasses.IAnnounce;
 import fr.sharedclasses.Product;
+import fr.uge.employee.Employee;
 
 public class NotificationEmailService {
 	@Autowired
     private JavaMailSender emailSender;
 	
-	public void sendSimpleMessage(String to, Product product ) {
+	public void sendSimpleMessage(Employee employee, IAnnounce announce) throws RemoteException {
 		        SimpleMailMessage message = new SimpleMailMessage(); 
 		        message.setFrom("noreply@eiffel_corp.com");
-		        message.setTo(to); 
-		        String subject = product.getLabel() + " is available on IfShare App !";
+		        message.setTo("test"); 
+		        String subject = announce.getLabel() + " is available on IfShare App !";
 		        String text = "You have registered to be notified when the product becomes available again.\n" 
-		                    + product.getLabel() + " is available now.\nIfShare App." ;
+		                    + announce.getLabel() + " is available now.\nIfShare App." ;
 		        message.setSubject(subject); 
 		        message.setText(text);
 		        emailSender.send(message);
